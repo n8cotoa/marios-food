d4664d5e3fc66d16285db6276c4cbf3ccfc3c9c9
Nate Cottle <cottle.nate@gmail.com>
2018-09-07 12:16:11 -0700

    seed file works with reviews and review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
-  # validates :author, :content_body, :rating, :presence => true
-  # validates :rating,  numericality: { only_integer: true, greater_than: 0, less_than: 6 }
-  # validates :content_body, length: {in: 50..250}
+  validates :author, :content_body, :rating, :presence => true
+  validates :rating,  numericality: { only_integer: true, greater_than: 0, less_than: 6 }
+  validates :content_body, length: {in: 50..250}
   belongs_to :product
 end
