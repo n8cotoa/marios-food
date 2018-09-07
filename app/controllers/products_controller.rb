cbcf527577b06890a8932ccebcf97406ffe8f3cb
Nate Cottle <cottle.nate@gmail.com>
2018-09-07 13:40:49 -0700

    build out crud functionality for products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,45 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def new
+  def show
+    @product = Product.find(params[:id])
+  end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :cost, :origin_country)
   end
 
 end
