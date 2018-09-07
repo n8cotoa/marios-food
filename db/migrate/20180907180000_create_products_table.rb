3b9fdd411dc0a756ebcc3ba97d3193bd49e58b1d
Nate Cottle <cottle.nate@gmail.com>
2018-09-07 11:09:52 -0700

    add migrations to create products and reviews tables in database.

--- db/migrate/20180907180000_create_products_table.rb
+++ db/migrate/20180907180000_create_products_table.rb
@@ -1,6 +1,9 @@
 class CreateProductsTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :products_tables do |t|
+    create_table :products do |t|
+      t.column :name, :string
+      t.column :cost, :integer
+      t.column :origin_country, :string
     end
   end
 end
